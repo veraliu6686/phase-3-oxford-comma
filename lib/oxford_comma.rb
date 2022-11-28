aac664f14ffcb04e54cff3c91927060b6d792617
Lianzi <lianziliu.sugar@gmail.com>
2022-11-28 15:25:20 -0500

    done

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,10 @@
+require "pry"
 def oxford_comma(array)
-
+    if array.length < 3
+        return array.join(" and ")
+    end
+    array[-1] = "and " + array[-1]
+    array.join(", ")
 end
+
+# binding.pry
